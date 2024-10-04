778e694c29aea240106de75a4b28cd8ac5f761d2
ST10053561 <ST10053561@vcconnect.edu.za>
2024-10-04 22:02:52 +0200

    dfghjkl

--- BACKEND/jest.backend.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*Regtest.js'],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.mjs$': 'babel-jest' // Add this line to handle .mjs files
-    },
-    transformIgnorePatterns: ['/node_modules/']
-  };
