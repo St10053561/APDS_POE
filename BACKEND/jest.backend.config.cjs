746e205b4ba4a129c86a4118896630954f039a22
ST10053561 <ST10053561@vcconnect.edu.za>
2024-10-05 20:26:57 +0200

    rtyujkl

--- BACKEND/jest.backend.config.cjs
+++ BACKEND/jest.backend.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['../tests/LoginRegtest.mjs'], // Updated pattern to match the new test file name
+  testMatch: ['**/tests/**/*.test.js'], // Updated pattern to match the new test file name
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.mjs$': 'babel-jest' // Add this line to handle .mjs files
